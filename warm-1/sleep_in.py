4be625495d1ffd79538d294385a10b5d8332e3d0
fuenshay <fuenshay@gmail.com>
2020-05-25 14:13:37 -0500

    Sleeping In

---
+++ warm-1/sleep_in.py
@@ -0,0 +1,12 @@
+# def sleep_in(weekday, vacation):
+#   if not weekday or vacation:
+#     return True
+#   else:
+#     return False
+    
+def sleep_in(weekday, vacation):
+    return(not weekday or vacation)
+    
+    
+    
+
