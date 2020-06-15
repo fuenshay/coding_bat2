37320b41601524131bed039b6a8e594144351386
fuenshay <fuenshay@gmail.com>
2020-06-15 03:37:47 -0500

    Make Bricks

---
+++ Logic-2/make_bricks.py
@@ -0,0 +1,21 @@
+"""
+
+Author: Shay Fu
+Date: June 13, 2020
+
+
+We want to make a row of bricks that is goal inches long. We have a number of small bricks (1 inch each) and big bricks (5 inches each). Return True if it is possible to make the goal by choosing from the given bricks. This is a little harder than it looks and can be done without any loops. See also: Introduction to MakeBricks
+
+
+make_bricks(3, 1, 8) → True
+make_bricks(3, 1, 9) → False
+make_bricks(3, 2, 10) → True
+
+"""
+
+def make_bricks(small, big, goal):
+  rem = goal % 5
+  sums = small + big*5
+  if sums >= goal and small >= rem:
+    return True
+  return False
