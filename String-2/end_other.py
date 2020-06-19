f7006ed119784348218ac7b769b560b5502fa27e
fuenshay <fuenshay@gmail.com>
2020-06-18 23:20:23 -0500

    End Other

---
+++ String-2/end_other.py
@@ -0,0 +1,25 @@
+"""
+Author: Shayna Fu
+Date: June 17, 2020
+
+
+Given two strings, return True if either of the strings appears
+at the very end of the other string, ignoring upper/lower case
+differences (in other words, the computation should not be "case
+sensitive"). Note: s.lower() returns the lowercase version of a string.
+
+
+end_other('Hiabc', 'abc') → True
+end_other('AbC', 'HiaBc') → True
+end_other('abc', 'abXabc') → True
+
+"""
+
+SOLUTION:
+
+def end_other(a, b):  
+ a = a.lower()
+ b = b.lower()
+ if a.endswith(b) or b.endswith(a):
+   return True
+ return False
