3080cc1fa79c15d70b0334305b8aa46c488c711a
fuenshay <fuenshay@gmail.com>
2020-06-18 23:12:25 -0500

    Count Code

---
+++ String-2/count_code.py
@@ -0,0 +1,23 @@
+"""
+Author: Shayna Fu
+Date: June 17, 2020
+
+
+Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+
+count_code('aaacodebbb') → 1
+count_code('codexxcode') → 2
+count_code('cozexxcope') → 2
+
+"""
+
+SOLUTION:
+
+def count_code(str):
+  count = 0
+  for i in range(len(str)-3):
+    if str[i:i+2] == "co" and str[i+3] == "e":
+      count += 1
+  return count
+
