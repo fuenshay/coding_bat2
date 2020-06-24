94f67c26b4573605eb87f65d2ec612e8114294e1
fuenshay <fuenshay@gmail.com>
2020-06-24 13:16:34 -0500

    Has 22

---
+++ List-2/has_22.py
@@ -0,0 +1,23 @@
+"""
+Author: Shayna Fu
+Date: June 23, 2020
+
+
+Given an array of ints, return True if the array contain
+s a 2 next to a 2 somewhere.
+
+
+has22([1, 2, 2]) → True
+has22([1, 2, 1, 2]) → False
+has22([2, 1, 2]) → False
+
+"""
+
+SOLUTION:
+
+def has22(nums):
+  for i in range (len(nums)-1):
+    if nums[i] == 2 and nums[i+1] == 2:
+      return True
+  return False
+
